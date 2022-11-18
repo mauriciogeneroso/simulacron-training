4903a54b7bd33997909a4a28f557cb8b9fa302f4
Mauricio Generoso <mauro_generoso@hotmail.com>
2022-11-18 10:17:49 +0000

    Use string on id instead UUID

--- training-app/src/main/java/com/generoso/training/simulacron/datarest/BookRepository.java
+++ training-app/src/main/java/com/generoso/training/simulacron/datarest/BookRepository.java
@@ -2,7 +2,5 @@ package com.generoso.training.simulacron.datarest;
 
 import org.springframework.data.cassandra.repository.CassandraRepository;
 
-import java.util.UUID;
-
-public interface BookRepository extends CassandraRepository<Book, UUID> {
+public interface BookRepository extends CassandraRepository<Book, String> {
 }
